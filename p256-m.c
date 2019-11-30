bd5eccfa48a66143cd8c2775b05577d5a975380e
Manuel Pégourié-Gonnard <mpg@elzevir.fr>
2019-11-30 11:32:32 +0100

    Add tests with randomly generated values

--- p256-m.c
+++ p256-m.c
@@ -2,6 +2,8 @@
 
 #if !defined(NO_MAIN)
 #include <stdio.h>
+#include <assert.h>
+#include <string.h>
 #endif
 
 #if defined(NO_MAIN)
@@ -61,6 +63,12 @@ STATIC uint32_t u256_sub(uint32_t z[8],
     return carry;
 }
 
+/**********************************************************************
+ *
+ * Functions and data for testing and debugging
+ *
+ **********************************************************************/
+
 #if !defined(NO_MAIN)
 static void print_u256(const char *name, const uint32_t x[8], uint32_t c)
 {
@@ -86,6 +94,45 @@ static void test_sub(const char *name,
     print_u256(name, z, c);
 }
 
+static const uint32_t r[8] = {
+    0xdcd1d063, 0x7d3d0eb8, 0x9c4ecc3c, 0xd937cbcb,
+    0x0a14613e, 0xf76db5ed, 0xec0db49c, 0x760cd745,
+};
+static const uint32_t s[8] = {
+    0x514595c2, 0xc5e403b2, 0x5444fc98, 0xb3b1c6ed,
+    0xaccbcfff, 0xdde65249, 0x120eb6d7, 0x17380bcf,
+};
+static const uint32_t rps[8] = {
+    0x2e176625, 0x4321126b, 0xf093c8d5, 0x8ce992b8,
+    0xb6e0313e, 0xd5540836, 0xfe1c6b74, 0x8d44e314,
+};
+static const uint32_t rms[8] = {
+    0x8b8c3aa1, 0xb7590b06, 0x4809cfa3, 0x258604de,
+    0x5d48913f, 0x198763a3, 0xd9fefdc5, 0x5ed4cb76,
+};
+static const uint32_t smr[8] = {
+    0x7473c55f, 0x48a6f4f9, 0xb7f6305c, 0xda79fb21,
+    0xa2b76ec0, 0xe6789c5c, 0x2601023a, 0xa12b3489,
+};
+
+static void assert_add(const uint32_t x[8], const uint32_t y[8],
+                       const uint32_t z[8], uint32_t c)
+{
+    uint32_t myz[8];
+    uint32_t myc = u256_add(myz, x, y);
+    assert(memcmp(myz, z, sizeof myz) == 0);
+    assert(myc == c);
+}
+
+static void assert_sub(const uint32_t x[8], const uint32_t y[8],
+                       const uint32_t z[8], uint32_t c)
+{
+    uint32_t myz[8];
+    uint32_t myc = u256_sub(myz, x, y);
+    assert(memcmp(myz, z, sizeof myz) == 0);
+    assert(myc == c);
+}
+
 int main(void)
 {
     uint32_t zero[8] = {0, 0, 0, 0, 0, 0, 0, 0};
@@ -93,6 +140,7 @@ int main(void)
     uint32_t mone[8] = {-1u, -1u, -1u, -1u, -1u, -1u, -1u, -1u};
     uint32_t word[8] = {-1u, 0, 0, 0, 0, 0, 0, 0};
 
+#if 0
     test_add("0+0", zero, zero);
     test_add("0+1", zero, one);
     test_add("0+m", zero, mone);
@@ -134,5 +182,15 @@ int main(void)
     test_sub("w-1", word, one);
     test_sub("w-m", word, mone);
     test_sub("w-w", word, word);
+#else
+    /* Just to keep the functions and variables used */
+    test_add("w+m", word, mone);
+    test_sub("0-1", zero, one);
+#endif
+
+    assert_add(r, s, rps, 0u);
+
+    assert_sub(r, s, rms, 0u);
+    assert_sub(s, r, smr, 1u);
 }
 #endif
---
+++ rnd.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+import random
+
+n_limbs = 8
+
+def c_print(name, val):
+    array = name + '[' + str(n_limbs) + ']'
+    print('static const uint32_t', array, '= {', end='')
+    for i in range(n_limbs):
+        sep = '\n    ' if i % 4 == 0 else ' '
+        print(sep + '0x' + format(val % 2**32, '08x') + ',', end='')
+        val >>=32
+    print('\n};')
+
+r = random.randrange(2**256)
+s = random.randrange(2**256)
+
+c_print('r', r)
+c_print('s', s)
+c_print('rps', r + s)
+c_print('rms', r - s)
+c_print('smr', s - r)
